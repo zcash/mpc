43a8296c8adfff68e229d4d017a21a93afdefea3
Sean Bowe <ewillbefull@gmail.com>
2016-09-27 12:49:51 -0600

    Add mpc "network" ISO.

--- build-iso.sh
+++ build-iso.sh
@@ -9,8 +9,12 @@ cd ../../
 sudo docker run --rm -it -v "$(pwd)":/home/rust/src mpc-musl-builder cargo build --release --bin compute --no-default-features
 cp target/x86_64-unknown-linux-musl/release/compute iso/mpc_compute/mpc_compute.rs
 
+sudo docker run --rm -it -v "$(pwd)":/home/rust/src mpc-musl-builder cargo build --release --bin network --no-default-features
+cp target/x86_64-unknown-linux-musl/release/network iso/mpc_network/mpc_network.rs
+
 cd iso
 sudo docker build -t mpc-iso .
 cd ..
 
 sudo docker run --rm -it -v "$(pwd)":/home/builder/target mpc-iso cp -L alpine-compute.iso /home/builder/target/alpine-compute.iso
+sudo docker run --rm -it -v "$(pwd)":/home/builder/target mpc-iso cp -L alpine-network.iso /home/builder/target/alpine-network.iso
---
+++ iso/alpine-network.conf.mk
@@ -0,0 +1,4 @@
+ALPINE_NAME     := alpine-network
+ALPINE_VERSION  := 1.0.0
+MODLOOP_EXTRA   := 
+KERNEL_FLAVOR   := grsec
